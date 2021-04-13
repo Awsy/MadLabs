2b64d173219c544e9f91f8615361b4b9ff9b0a73
Volodymyr Boiko <vladimir.boiko@socialtechnologies.io>
2021-04-13 12:29:33 +0300

    updates

--- MadLibs/AltStoryView.swift
+++ MadLibs/AltStoryView.swift
@@ -1,22 +1,18 @@
-
 import UIKit
 
-
 class AltStoryView: UIViewController {
-    
-    @IBOutlet weak var altStoryLabel: UILabel!
-    
-    var altStory: String = ""
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        altStoryLabel.text = altStory
-    }
-    
-    @IBAction func closeAltModal(_ sender: UIButton) {
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+  @IBOutlet weak var altStoryLabel: UILabel!
+
+  var altStory: String = ""
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    altStoryLabel.text = altStory
+  }
+
+  @IBAction func closeAltModal(_ sender: UIButton) {
+
+    self.dismiss(animated: true, completion: nil)
+  }
 }
